d6ee4bdc1f4d6e6562a55c867fc7dced14cd89ca
Tanner Woodworth <twoodworth2014@gmail.com>
2016-11-01 23:45:00 -0400

    Added some sexy nav stuff

---
+++ _site/_build/js/scripts.js
@@ -0,0 +1,11 @@
+// Library Imports
+// ===================================
+
+// @include-static lib/jquery/jquery-3.1.1.min.js
+
+
+// Site Specific Imports
+// ===================================
+
+// @include-static site/site.js
+
