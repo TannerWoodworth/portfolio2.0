8e5d2d40e99367b5525995d9dcbf33ebcf6b0359
Tanner Woodworth <twoodworth2014@gmail.com>
2017-02-17 18:17:46 -0500

    Started new site

---
+++ site/_build/js/scripts.js
@@ -0,0 +1,11 @@
+// Library Imports
+// ===================================
+
+// @include-static lib/jquery/jquery-3.1.1.min.js
+
+
+// Site Specific Imports
+// ===================================
+
+// @include-static lib/modaal/modaal.min.js
+// @include-static site/site.js
